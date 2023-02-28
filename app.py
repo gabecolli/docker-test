ec012ff5f4e979a35cfdb37a6df925ac4ff4fec2
Gabriel Colli <gabrielcolli@microsoft.com>
2023-02-28 15:33:19 -0600

    stripped the dashes for the envvar

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import os
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("index.html",testenv=os.environ.get('testenvvar'))
+    testvar = os.environ.get('testenvvar')
+    formatted_env_var = testvar.replace("-", " ")
+    return render_template("index.html",testenv=formatted_env_var)
 
 
 
 if __name__ ==("__main__"):
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
